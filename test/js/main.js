1ffb82c9b2c3545db0876c5d0fe60c5305fefaf5
David <estilgrafic@gmail.com>
2017-12-02 18:45:22 +0800

    Uploaded /test for test purposes...

---
+++ test/js/main.js
@@ -0,0 +1,28 @@
+$( document ).ready(function() {
+
+	// Scroll Down Arrow
+
+	$("#scroll-arrow").click(function() {
+	    $('html, body').animate({
+	        scrollTop: $("#story").offset().top -100
+	    }, 800);
+	});
+  $(window).bind("mousewheel", function() {
+     $("html, body").stop(true, false);;
+  });
+
+  // ///////////////////////////////////////////////////////////////
+
+  // Trigger Animations On Scroll
+
+	var watchers = $('.faded').map(function(i, element) {
+
+		var watcher = scrollMonitor.create( element, -200);
+		watcher.enterViewport(function() {
+				$(element).addClass('fadeInUp')
+		});
+
+	});
+
+});
+
