6c52efb141b962535829a4d0e077249801561b34
David G <estilgrafic@gmail.com>
2019-05-02 12:16:59 +0200

    - Better structure with detached dev code from production - Gulpfile now works with the new structure

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,129 @@
-var gulp = require('gulp');  
-var sass = require('gulp-sass');  
-var browserSync = require('browser-sync');
-const autoprefixer = require('gulp-autoprefixer');
+var gulp = require("gulp"),
+    sass = require("gulp-sass"),
+    postcss = require("gulp-postcss"),
+    autoprefixer = require("autoprefixer"),
+    cssnano = require("cssnano"),
+    concat = require("gulp-concat"),
+    terser = require("gulp-terser"),
+    del = require('del'),
+    image = require('gulp-image'),
+    sourcemaps = require("gulp-sourcemaps");
 
-var input = './scss/main.scss';
-var output = './css';
+var browserSync = require("browser-sync").create();
+
+// Paths to all files needed
+var paths = {
+  styles: {
+    src: "./dev/scss/main.scss",
+    srcWatch: "./dev/scss/**/*.scss",
+    dest: "./docs/css"
+  },
+  markup: {
+    src: ["./dev/**/*.html"],
+    srcWatch: ["./dev/**/*.html", "./dev/js/**/*.js"],
+    dest: "./docs/"
+  },
+  scripts: {
+    src: ["./dev/js/plugins.js", "./dev/js/modernizr.min.js", "./dev/js/main.js"],
+    srcWatch: "./dev/js/**/*.js",
+    dest: "./docs/js"
+  },
+  imgs: {
+    src: "./dev/img/**/*",
+    srcWatch: "./dev/img/**/*",
+    dest: "./docs/img"
+  }
+
+}
+
+// Transpile, concatenate and minify scripts
+function markup() {
+  return (
+    gulp
+    .src(paths.markup.src)
+    .pipe(gulp.dest(paths.markup.dest))
+  );
+}
 
 
-gulp.task('browser-sync', function() {  
-    browserSync.init(["../*/*.html", "css/*.css", "js/*.js"], {
-        server: {
-            baseDir: "./"
-        }
-    });
-});
+// SASS to CSS compilation with sourcemaps
+function styles() {
+  return (
+    gulp
+    .src(paths.styles.src)
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+    .on("error", sass.logError)
+    .pipe(postcss([autoprefixer(), cssnano()]))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(paths.styles.dest))
+    .pipe(browserSync.stream())
+    );
+}
 
-gulp.task('sass', function () {
-  gulp.src(input)
-    .pipe(sass({
-      outputStyle: 'compressed'
-    }).on('error', sass.logError))
-    .pipe(autoprefixer({
-      browsers: ['last 4 versions', 'android 4'],
-      cascade: false
+// Transpile, concatenate and minify scripts
+function scripts() {
+  return (
+    gulp
+    .src(paths.scripts.src)
+    .pipe(concat('main.min.js'))
+    .pipe(terser())
+    .pipe(gulp.dest(paths.scripts.dest))
+  );
+}
+
+// Optimize images and copy them into docs
+function images() {
+  return (
+    gulp
+    .src(paths.imgs.src)
+    .pipe(image({
+      zopflipng: false
     }))
-    .pipe(gulp.dest(output))
-    .pipe(browserSync.stream());
-});
+    .pipe(gulp.dest(paths.imgs.dest))
+    );
+}
+
+//Clean all auto-generated code from docs
+function clean() {
+  return del([
+    './docs/css',
+    './docs/img',
+    './docs/js',
+    './docs/index.html'
+  ]);
+}
+
+//Task to reload the page if something change
+function reload(done) {
+  browserSync.reload();
+  done();
+}
+
+// Watch for changes
+function watch(){
+
+  browserSync.init({
+    server: {
+      baseDir: "./docs"
+    }
+  });
+
+  gulp.watch(paths.styles.srcWatch, styles);
+  gulp.watch(paths.imgs.srcWatch, images);
+  gulp.watch(paths.scripts.srcWatch, scripts);
+  gulp.watch(paths.markup.srcWatch, gulp.series(markup, reload));
+}
 
 
+//Build task
+var build = gulp.series(clean, markup, gulp.parallel(images, styles, scripts), watch);
 
-gulp.task('default', ['browser-sync', 'sass'], function () {  
-    gulp.watch("./scss/*/*.scss", ['sass']);
-});
+//export tasks
+exports.clean = clean;
+exports.markup = markup;
+exports.styles = styles;
+exports.scripts = scripts;
+exports.images = images;
+exports.watch = watch;
+exports.default = build;
